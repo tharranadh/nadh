3ceb3acf5498621c5d7412b293ef74ce97f26ead
tharranadh <tharranadh@gmail.com>
2026-05-31 08:55:56 +0700

    Update login.js

--- login/js/login.js
+++ login/js/login.js
@@ -3,10 +3,16 @@ document.getElementById("loginForm").addEventListener("submit", function(e) {
     const username = document.getElementById("username").value.trim();
     const password = document.getElementById("password").value.trim();
 
-    if (username === "heri" && password === "123") {
+    const savedUsername = localStorage.getItem("reg_username");
+    const savedPassword = localStorage.getItem("reg_password");
+
+    if (username === savedUsername && password === savedPassword) {
         localStorage.setItem("username", username);
         window.location.href = "/nadh/index.html";
     } else {
-        alert("Username atau Password salah!");
+        const alertBox = document.getElementById("alertBox");
+        alertBox.innerText = "Username atau Password salah!";
+        alertBox.style.display = "block";
+        setTimeout(() => alertBox.style.display = "none", 3000);
     }
 });
